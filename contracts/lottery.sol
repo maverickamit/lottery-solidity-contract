4153b0ce60cf2ac2121c650ab858f73bdc131c78
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-12-26 16:12:18 +0530

    feat: implement enter function to add players - store addresses of players

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -2,10 +2,18 @@ pragma solidity ^0.4.17;
 
 contract Lottery {
     address public manager;
-    
+    address[] public players;
+
     //storing address of contract creator as manager
     function Lottery() public {
+        
         manager = msg.sender;
     }
+
+    //storing address of players
+    function enter() public payable {
+        players.push(msg.sender);
+    }
+}
     
 }
