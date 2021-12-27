cbb2ca6818ad92f4930f6436054e37a600146a13
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-12-27 12:06:14 +0530

    test: add test to verify one player can enter

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,3 +1,4 @@
+const { strictEqual } = require("assert");
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
@@ -18,5 +19,19 @@ beforeEach(async () => {
 describe("Lottery Contract", () => {
   it("can deploy a contract", () => {
     assert.ok(lottery.options.address);
+  });
+
+  it("allows one player to enter", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("0.02", "ether"),
+    });
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    strictEqual(accounts[0], players[0]);
+    strictEqual(1, players.length);
   });
 });
