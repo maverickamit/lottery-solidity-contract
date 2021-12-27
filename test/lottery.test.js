4e782b33ca0e7d29d9c618a3aa260554f6b8020e
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2021-12-27 18:42:43 +0530

    feat: add a new test verify after picking winner - money is sent to the winner - players array is reset - contract balance is zero

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -87,4 +87,31 @@ describe("Lottery Contract", () => {
       assert(error.results);
     }
   });
+
+  it("sends money to the winner and resets the players array and contract balance", async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei("2", "ether"),
+    });
+    const initialBalance = await web3.eth.getBalance(accounts[0]);
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+    const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;
+
+    //verifies money is sent to the winner
+    assert(difference > web3.utils.toWei("1.8", "ether"));
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0],
+    });
+
+    //verifies players array is reset
+    assert(players.length == 0);
+    const contractBalance = await web3.eth.getBalance(lottery._address);
+
+    //verifies contract balance is reset
+    assert(contractBalance == 0);
+  });
 });
